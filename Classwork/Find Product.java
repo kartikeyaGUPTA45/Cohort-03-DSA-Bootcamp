da1a75fc7e28997e0990153c180ce9d564ebd891
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-04-06 22:55:31 +0530

    Create Find Product.java

---
+++ Classwork/Find Product.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner (System.in);
+
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        int d = scn.nextInt();
+        int e = scn.nextInt();
+
+        int ans = a*b*c*d*e;
+
+        System.out.println(ans);
+    }
+}
