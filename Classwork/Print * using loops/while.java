2058257018d9e53404b15b389d3d31ab465406f9
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-04-14 10:11:37 +0530

    Create while.java

---
+++ Classwork/Print * using loops/while.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int i =1;
+        while(i<=n) {
+            System.out.println("*");
+            i++;
+        }
+    }
+}
