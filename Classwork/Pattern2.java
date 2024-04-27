32a6c7b1748cdc31b61bd0e2ebe4421e68668877
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-04-27 21:27:17 +0530

    Create Pattern2.java

---
+++ Classwork/Pattern2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int nst = n;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=nst;j++) {
+                System.out.print("*\t");
+            }
+            nst--;
+            System.out.println();
+        }
+    }
+}
