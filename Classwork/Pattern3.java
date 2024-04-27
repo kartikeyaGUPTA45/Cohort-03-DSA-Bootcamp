cab18902686b946c724a1259e8af3aefde368157
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-04-27 21:51:50 +0530

    Create Pattern3.java

---
+++ Classwork/Pattern3.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      
+      int nst = 1, nsp = n-1;
+      
+      for(int i=1;i<=n;i++) {
+        for(int j=1;j<=nsp;j++) {
+          System.out.print("\t");
+        }
+        
+        for(int j=1;j<=nst;j++) {
+          System.out.print("*\t");
+        }
+        
+        nst++;
+        nsp--;
+        System.out.println();
+      }
+    }
+}
