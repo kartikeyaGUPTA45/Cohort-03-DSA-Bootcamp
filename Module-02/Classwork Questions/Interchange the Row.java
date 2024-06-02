f9bdd072ae5f7c98ca8b970fedbe2a8f6acb4233
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-06-02 10:01:28 +0530

    Create Interchange the Row.java

---
+++ Module-02/Classwork Questions/Interchange the Row.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+   public static void interchangeRow(int arr[][], int m, int n) {
+     int zerothRow = 0, lastRow = m-1;
+     
+     for(int k=0;k<n;k++) {
+       int temp = arr[zerothRow][k];
+       arr[zerothRow][k] = arr[lastRow][k];
+       arr[lastRow][k] = temp;
+     }
+     
+     for(int i=0;i<m;i++) {
+       for(int j=0;j<n;j++) {
+         System.out.print(arr[i][j] + " ");
+       }
+       System.out.println();
+     }
+   }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scn = new Scanner(System.in);
+      int m = scn.nextInt();
+      int n = scn.nextInt();
+      
+      int arr[][] = new int [m][n];
+      
+      for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+          arr[i][j] = scn.nextInt();
+        }
+      }
+      
+      interchangeRow(arr,m,n);
+    }
+}
