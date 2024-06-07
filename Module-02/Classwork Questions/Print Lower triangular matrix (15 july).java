03bf89301688401f1562ce06e964ea6aa3f8e7f4
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-06-07 21:25:19 +0530

    Create Print Lower triangular matrix (15 july).java

---
+++ Module-02/Classwork Questions/Print Lower triangular matrix (15 july).java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void printLowerTriangularMatrix(int arr[][], int m, int n) {
+      for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+          if (i>=j) {
+            System.out.print(arr[i][j] + " ");
+          } else {
+            System.out.print(0 + " ");
+          }
+        }
+        System.out.println();
+      }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scn = new Scanner(System.in);
+      int m = scn.nextInt();
+      int n = scn.nextInt();
+      
+      int arr[][] = new int [m][n];
+      
+      for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+          arr[i][j] = scn.nextInt();
+        }
+      }
+      
+      printLowerTriangularMatrix(arr,m,n);
+    }
+}
