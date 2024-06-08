0eb65f517b9c5c12ffcbb423766cda5ab9c4afcd
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-06-08 22:13:38 +0530

    Create Rotate By 90 Degree (15 July).java

---
+++ Module-02/Classwork Questions/Rotate By 90 Degree (15 July).java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+  
+    public static void displayArray(int arr[][], int n) {
+      for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+          System.out.print(arr[i][j] + " ");
+        }
+        System.out.println();
+      }
+    }
+  
+    public static void swapRowise(int arr[][], int n) {
+      for(int i=0;i<n;i++) {
+        int left = 0, right = n-1;
+        while(left<right) {
+          int temp = arr[i][left];
+          arr[i][left] = arr[i][right];
+          arr[i][right] = temp;
+          
+          right--;
+          left++;
+        }
+        
+      }
+    }  
+  
+    public static void swap(int arr[][], int i, int j) {
+      int temp = arr[i][j];
+      arr[i][j] = arr[j][i];
+      arr[j][i] = temp;
+    }
+  
+    public static void  transposeMatrix(int arr[][], int n) {
+      for(int i=1;i<n;i++) {
+        for(int j=0;j<i;j++) {
+          
+          swap(arr,i,j);
+        }
+      }
+      
+      
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int arr[][] = new int[n][n];
+      for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+          arr[i][j] = scn.nextInt();
+        }
+      }
+      
+        transposeMatrix(arr,n);
+        swapRowise(arr,n);
+        displayArray(arr,n);
+    }
+}
