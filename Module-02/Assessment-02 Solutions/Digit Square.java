60aeeb676517e80c489d30ca7235cd5d922b5495
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-06-16 09:31:11 +0530

    Update Digit Square.java

--- Module-02/Assessment-02 Solutions/Digit Square.java
+++ Module-02/Assessment-02 Solutions/Digit Square.java
@@ -21,24 +21,7 @@ Input: 23
 Output: 49
 
 
-Solution -01 (When return type is int) 
-
-public static int DigitSquare(int num) {
-    // code goes here  
-    // num = 12
-    int num1 = num%10; // 2
-    num/=10;
-    int num2 = num%10; // 1
-
-    num1 = num1 * num1;
-    num2 = num2 * num2;
-
-    int ans = num2*10+num1;
-    return ans; 
-  }
-
-
-Solution -02 (When return type is String) 
+Solution:
 public static String DigitSquare(int num) {
     // code goes here  
     // num = 12
