825e4e3fe2ceb55520c9c2fc9cff17ffdafe7e60
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-06-21 22:24:57 +0530

    Create Compare Two Matrices.java

---
+++ Module-02/Homework Questions/Compare Two Matrices.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+  
+    public static String compareTwoMatrices(int a1[][], int a2[][], int m1, int n1, int m2, int n2) {
+      if (m1 != m2 || n1 != n2) {
+        return "Not Same";
+      }
+      
+      for(int i=0;i<m1;i++) {
+        for(int j=0;j<n2;j++) {
+          if(a1[i][j] != a2[i][j]) {
+            return "Not Same";
+          }
+        }
+      }
+      
+      return "Same";
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scn = new Scanner(System.in);
+      int m1 = scn.nextInt();
+      int n1 = scn.nextInt();
+      
+      int a1[][] = new int[m1][n1];
+      for(int i=0;i<m1;i++) {
+        for(int j=0;j<n1;j++) {
+          a1[i][j] = scn.nextInt();
+        }
+      }
+      
+      int m2 = scn.nextInt();
+      int n2 = scn.nextInt();
+      
+      int a2[][] = new int[m2][n2];
+      for(int i=0;i<m2;i++) {
+        for(int j=0;j<n2;j++) {
+          a2[i][j] = scn.nextInt();
+        }
+      }
+      
+      System.out.println(compareTwoMatrices(a1,a2,m1,n1,m2,n2));
+    }
+}
