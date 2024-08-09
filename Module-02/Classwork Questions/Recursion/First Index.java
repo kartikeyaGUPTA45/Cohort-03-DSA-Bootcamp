9f9d68bd414aaa0764dede6dd44ce68644162594
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-08-09 21:59:33 +0530

    Create First Index.java

---
+++ Module-02/Classwork Questions/Recursion/First Index.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static int firstIdx(int arr[], int x, int n, int idx) {
+      if (idx == n) {
+        return -1;
+      }
+      
+      if (arr[idx] == x) {
+        return idx;
+      } else {
+        return firstIdx(arr,x,n,idx+1);
+      }
+    } 
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int arr[] = new int[n];
+      for(int i=0;i<n;i++) {
+        arr[i] = scn.nextInt();
+      }
+      
+      int x = scn.nextInt();
+      
+      System.out.println(firstIdx(arr,x,n,0));
+    }
+}
