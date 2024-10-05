15010d1c46abe3c55d2d9f917c35fccea39ea460
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-10-05 21:59:59 +0530

    Update recursive-pre-post-and-inorder-traversals-of-binary-tree.java

--- Module-02/Classwork Questions/Tree/recursive-pre-post-and-inorder-traversals-of-binary-tree.java
+++ Module-02/Classwork Questions/Tree/recursive-pre-post-and-inorder-traversals-of-binary-tree.java
@@ -80,15 +80,33 @@ public class Main {
   }
     
   public static void preOrder(Node node){
+      if (node == null) {
+        return;
+      }
       
+      System.out.print(node.data + " ");
+      preOrder(node.left);
+      preOrder(node.right);
   }
     
   public static void inOrder(Node node){
+      if (node == null) {
+        return;
+      }
       
+      inOrder(node.left);
+      System.out.print(node.data + " "); 
+      inOrder(node.right);
   }
     
   public static void postOrder(Node node){
+      if (node == null) {
+        return;
+      }
       
+      postOrder(node.left);
+      postOrder(node.right);
+      System.out.print(node.data + " ");
   }  
 
 
