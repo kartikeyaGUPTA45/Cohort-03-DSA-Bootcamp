7cae64f0dc925f3ea253748ca91cdf51d82444f3
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-10-19 22:26:51 +0530

    Update Search in a Binary Search Tree.java

--- Module-02/Classwork Questions/Tree/BST/Search in a Binary Search Tree.java
+++ Module-02/Classwork Questions/Tree/BST/Search in a Binary Search Tree.java
@@ -84,7 +84,19 @@ public class Main {
   }
   
   public static Node searchBST(Node root, int val) {
-   
+     if (root == null) {
+       return null;
+     }
+    
+    if (root.data == val) {
+      return root;
+    }
+    
+    if (root.data > val) {
+      return searchBST(root.left, val);
+    } else {
+      return searchBST(root.right, val);
+    }
   }
   
   public static void main(String[] args) throws Exception {
