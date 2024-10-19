70e15da5b7fb4479b091834f4acf0a3757a34c91
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-10-19 21:51:35 +0530

    Update Size, Sum, Max, Min, Find In Bst.java

--- Module-02/Classwork Questions/Tree/BST/Size, Sum, Max, Min, Find In Bst.java
+++ Module-02/Classwork Questions/Tree/BST/Size, Sum, Max, Min, Find In Bst.java
@@ -80,23 +80,54 @@ public class Main {
   }
 
   public static int size(Node node) {
+    if (node == null) return 0;
+    int leftSize = size(node.left);
+    int rightSize = size(node.right);
+    
+    return leftSize + rightSize + 1;
     
   }
 
   public static int sum(Node node) {
+    if (node == null) return 0;
+    int leftSum = sum(node.left);
+    int rightSum = sum(node.right);
     
+    return leftSum + rightSum + node.data;
   }
 
   public static int max(Node node) {
+    if (node == null) return Integer.MIN_VALUE;
     
+    if (node.left == null && node.right == null) {
+      return node.data;
+    }
+    
+    return max(node.right);
   }
 
   public static int min(Node node) {
-
+    if (node == null) return Integer.MAX_VALUE;
+    
+    if (node.left == null && node.right == null) {
+      return node.data;
+    }
+    
+    return min(node.left);
   }
 
   public static boolean find(Node node, int data) {
-      
+      if (node == null) return false;
+    
+      if (node.data == data) {
+        return true;
+      }
+    
+      if (node.data < data) {
+        return find(node.right, data);
+      } else {
+        return find(node.left, data);
+      }
   }  
 
   public static void main(String[] args) throws Exception {
