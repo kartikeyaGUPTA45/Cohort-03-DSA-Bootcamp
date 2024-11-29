b1a31f6a7f1fedf62cefa9c5fd6acddaba33254a
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2024-11-29 22:54:03 +0530

    Create Count Of All Subarrays With Zero Sum.java

---
+++ Module-02/Classwork Questions/HashMap/Count Of All Subarrays With Zero Sum.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+  
+    public static int countSubarray(int arr[], int n) {
+      int ans = 0, sum = 0;
+      HashMap<Integer,Integer> map = new HashMap<>();
+      
+      map.put(0,1);
+      
+      for(int i=0;i<n;i++) {
+        sum+=arr[i];
+        
+        if (map.containsKey(sum)) {
+          ans += map.get(sum);
+          map.put(sum, map.get(sum)+1);
+        } else {
+          map.put(sum,1);
+        }
+      }
+      
+      return ans;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int arr[] = new int[n];
+      
+      for(int i=0;i<n;i++) {
+        arr[i] =  scn.nextInt(); 
+      }
+      
+      System.out.println(countSubarray(arr,n));
+    }
+}
