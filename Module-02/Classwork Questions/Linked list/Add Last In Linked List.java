e6b5f44c557875d63b2ee6dc8b5072e736f29aab
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-01-10 21:06:07 +0530

    Create Add Last In Linked List.java

---
+++ Module-02/Classwork Questions/Linked list/Add Last In Linked List.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+      // write code here
+    }
+  }
+
+  public static void testList(LinkedList list) {
+    for (Node temp = list.head; temp != null; temp = temp.next) {
+      System.out.println(temp.data);
+    }
+    
+  }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    LinkedList list = new LinkedList();
+
+    String str = br.readLine();
+    while(str.equals("exit") == false){
+      if(str.startsWith("addLast")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addLast(val);
+      } 
+      str = br.readLine();
+    }
+
+    testList(list);
+  }
+}    
