f41c4d5edc5f8b55b994ed7de40ec56c8d3be5b4
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-01-20 22:48:43 +0530

    Update K-th Bit is Set or Not.java

--- Module-02/Classwork Questions/Bits Manipulation/K-th Bit is Set or Not.java
+++ Module-02/Classwork Questions/Bits Manipulation/K-th Bit is Set or Not.java
@@ -1,1 +1,9 @@
 Problem Link: https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+
+solution 
+
+static boolean checkKthBit(int n, int k) {
+        // return ((n&(1<<k)) > 0); // Left Shift Operator
+        return (((n>>k)&1) == 1); // Right shift Operator
+}
