9a0665581d71ce6f6fc2716b5c11118cd54769ce
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-01-24 21:18:45 +0530

    Update Set kth bit.java

--- Module-02/Classwork Questions/Bits Manipulation/Set kth bit.java
+++ Module-02/Classwork Questions/Bits Manipulation/Set kth bit.java
@@ -1,1 +1,12 @@
 Problem Link: https://www.geeksforgeeks.org/problems/set-kth-bit3724/1
+
+
+Solution: 
+class Solution {
+    static int setKthBit(int n, int k) {
+        // code here
+        // 1010
+        // 1110 = 14
+        return (n|(1<<k)); 
+    }
+}
