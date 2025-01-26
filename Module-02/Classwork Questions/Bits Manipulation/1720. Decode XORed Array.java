e95fdcccfe01daf41fc1dd133e26658ec7cebca5
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-01-26 10:50:47 +0530

    Create 1720. Decode XORed Array.java

---
+++ Module-02/Classwork Questions/Bits Manipulation/1720. Decode XORed Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int ans [] = new int[encoded.length+1];
+        ans[0] = first;
+
+        for(int i=0;i<encoded.length;i++) {
+            ans[i+1] = encoded[i] ^ ans[i]; 
+        }
+
+        return ans;
+    }
+}
+
+ 
